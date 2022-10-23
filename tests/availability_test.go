e69febd465b91ca5692d78726eeb7a9917a9bccf
Pouya Vedadiyan <vedadiyan@gmail.com>
2022-10-23 12:41:13 +0800

    Unit Tests

---
+++ tests/availability_test.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	unmarshaller "github.com/vedadiyan/proton/pkg/unmarshaller"
+)
+
+func TestAvailability(t *testing.T) {
+	var mapper map[string]any
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error("cloud not get working directory")
+	}
+	file, err := os.ReadFile(fmt.Sprintf("%s/resources/availability.json", wd))
+	if err != nil {
+		t.Error("could not read source file")
+	}
+	err = json.Unmarshal(file, &mapper)
+	if err != nil {
+		t.Error("could not deserialize json")
+	}
+	activities := ArrayOfAvailabilities{}
+	err = unmarshaller.Unmarshall(mapper, &activities)
+	if err != nil {
+		t.Error(err)
+	}
+	len := len(activities.GetAvailabilities())
+	if len == 0 {
+		t.Error("bad unmarshaller")
+	}
+	t.Logf("%d unmarshalled", len)
+	availability := activities.GetAvailabilities()[1]
+	if availability.Id != "C BATLLO" {
+		t.Error("invalid id")
+	}
+	if availability.Name != "Casa Batlló - Flexi Ticket" {
+		t.Error("invalid name")
+	}
+	if availability.MinPrice != 33.33 {
+		t.Error("invalid min price")
+	}
+	if availability.Thumbnail != "https://media.activitiesbank.com/11003/ENG/L/11003_5.jpg" {
+		t.Error("invalid thumbnail")
+	}
+	if availability.Location.Longitude == 0 {
+		t.Error("invalid location")
+	}
+	if availability.Location.Longitude == 0 {
+		t.Error("invalid location")
+	}
+}
