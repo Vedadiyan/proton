662462cd0deff634b243a4fdf73457a8b88d511a
Pouya Vedadiyan <vedadiyan@gmail.com>
2022-10-23 13:22:09 +0800

    Unit Tests

--- tests/resources/generate.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p file_path=".Proto File Path: "
-set /p out_dir="Output Directory: "
-@echo on
-protoc --plugin=protoc-gen-protogenic=./protogenic.exe --go_out=./ --csharp_out=./  %file_path% --protogenic_out=%out_dir%
-pause
